6333f2fb23b8c950dc31c4fbfa595f57d85e1057
rikucame <rikucame@gmail.com>
2020-03-10 02:15:34 +0900

    Routerでlocationを取得できるようにした

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,18 +1,11 @@
 import React from 'react'
-import { BrowserRouter, Route} from 'react-router-dom';
-import Top from '../components/pages/Top';
-import About from '../components/pages/About';
-import Photos from '../components/pages/Photos';
-import Contact from '../components/pages/Contact';
-import {ROOT, ABOUT, PHOTOS, CONTACT} from './PagePath';
+import {BrowserRouter} from 'react-router-dom';
+import PageRouter from './PageRouter';
 
 export default () => {
     return(
         <BrowserRouter>
-            <Route exact path={ROOT} component={Top} />
-            <Route path={ABOUT} component={About} />
-            <Route path={PHOTOS} component={Photos} />
-            <Route path={CONTACT} component={Contact} />
+            <PageRouter />
         </BrowserRouter>
     )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './scss/index.scss';
 import * as serviceWorker from './serviceWorker';
 import Router from './Router';
-
 ReactDOM.render(<Router />, document.getElementById('root'));
 
 serviceWorker.unregister();
