b0e7f56c430e7a35cde78997afbc8e557f0e6400
rikucame <rikucame@gmail.com>
2020-03-29 23:57:50 +0900

    前のcommitにaddし忘れた

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./scss/index.scss";
 import * as serviceWorker from "./serviceWorker";
 import Router from "./Router";
+import "./scss/index.scss";
+
 ReactDOM.render(<Router />, document.getElementById("root"));
 
 serviceWorker.unregister();
